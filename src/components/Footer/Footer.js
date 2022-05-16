fb8478e14da06d896401c6250e56333090053fde
Mauricio Coronel <cmauriciocoronel@gmail.com>
2022-05-16 09:09:22 -0300

    Issue #86: Ajustar redes sociales

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,22 +24,42 @@ class Footer extends Component {
           <Grid item xs={8}>
             <img src={require("assets/img/logos/avaldao-alt.svg")} alt={t('give4forest')} className={classes.dappLogo} />
           </Grid>
-          <Grid item xs={8} style={{marginBottom: "1em", textAlign: "right"}}>
-          <SocialIcon url="https://www.facebook.com/ACDIargentina%20/"
+          <Grid item xs={8} style={{ marginBottom: "1em", textAlign: "right" }}>
+            {/*<SocialIcon url="https://www.facebook.com/ACDIargentina%20/"
               className={classes.socialMediaIcon}
               bgColor="#FFF"
               color="#7868E5"
-              target="_blank" rel="noopener noreferrer"/>
-            <SocialIcon url="https://www.instagram.com/acdiargentina/"
+              target="_blank" rel="noopener noreferrer" />*/}
+            {/*<SocialIcon url="https://www.instagram.com/acdiargentina/"
               className={classes.socialMediaIcon}
               bgColor="#FFF"
               color="#7868E5"
-              target="_blank" rel="noopener noreferrer"/>
-            <SocialIcon url="https://twitter.com/ACDIargentina"
+              target="_blank" rel="noopener noreferrer" />*/}
+            {/*<SocialIcon url="https://twitter.com/ACDIargentina"
               className={classes.socialMediaIcon}
               bgColor="#FFF"
               color="#7868E5"
-              target="_blank" rel="noopener noreferrer"/>
+              target="_blank" rel="noopener noreferrer" />*/}
+            {/*<SocialIcon url="https://www.linkedin.com/company/acdiargentina"
+              className={classes.socialMediaIcon}
+              bgColor="#FFF"
+              color="#7868E5"
+              target="_blank" rel="noopener noreferrer" />*/}
+            <SocialIcon url="https://twitter.com"
+              className={classes.socialMediaIcon}
+              bgColor="#FFF"
+              color="#7868E5"
+              target="_blank" rel="noopener noreferrer" />
+            <SocialIcon url="https://www.linkedin.com"
+              className={classes.socialMediaIcon}
+              bgColor="#FFF"
+              color="#7868E5"
+              target="_blank" rel="noopener noreferrer" />
+            <SocialIcon url="https://telegram.org"
+              className={classes.socialMediaIcon}
+              bgColor="#FFF"
+              color="#7868E5"
+              target="_blank" rel="noopener noreferrer" />
           </Grid>
           <Grid item xs={8}>
             <h6 className={classes.disclaimer}>
