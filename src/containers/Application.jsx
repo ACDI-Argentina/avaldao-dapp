4900c2717e0cf5ce2518bfa41991e0ef6e61f6ab
Jonatan E. Duttweiler <jonatanduttweiler@gmail.com>
2024-10-07 19:06:45 -0300

    Remove unused dependencies and udpate react-ga

--- src/containers/Application.jsx
+++ src/containers/Application.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+
 import { Router } from 'react-router-dom';
 import localforage from 'localforage';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import Sweetalert from 'sweetalert';
-import GA from 'lib/GoogleAnalytics';
+
+import GoogleAnalytics from 'lib/GoogleAnalytics';
 import { ScrollToTop } from '../lib/helpers';
 import config from '../configuration';
 import { ErrorBoundary, history, NavigateAnchor } from '@acdi/efem-dapp';
@@ -23,20 +23,12 @@ import TransactionViewer from 'components/TransactionViewer';
 import Web3Banner from 'lib/blockchain/Web3Banner';
 import Web3App from 'lib/blockchain/Web3App';
 import { Web3AppContext } from 'lib/blockchain/Web3App';
+import { Route } from 'react-router-dom';
 
 /* global document */
 /**
  * Here we hack to make stuff globally available
  */
-// Make sweet alert global
-React.swal = Sweetalert;
-
-// Construct a dom node to be used as content for sweet alert
-React.swal.msg = reactNode => {
-  const wrapper = document.createElement('span');
-  ReactDOM.render(reactNode, wrapper);
-  return wrapper.firstChild;
-};
 
 // make toast globally available
 React.toast = toast;
@@ -79,9 +71,9 @@ class Application extends Component {
 
   render() {
     const { currentUser } = this.props;
-    
+
     NavigateAnchor(history);
-    
+
     return (
       <ErrorBoundary>
         <Web3App>
@@ -97,7 +89,7 @@ class Application extends Component {
                 <Router history={history} >
                   <ScrollToTop />
                   <div>
-                    {GA.init() && <GA.RouteTracker />}
+                    <Route render={() => <GoogleAnalytics />} />
 
                     <SwitchRoutes currentUser={currentUser} />
 
--- src/lib/GoogleAnalytics.jsx
+++ src/lib/GoogleAnalytics.jsx
@@ -1,78 +1,58 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useRef } from 'react';
 import ReactGA from 'react-ga';
-import { Route } from 'react-router-dom';
 import config from '../configuration';
+import { useLocation } from 'react-router-dom';
 
-class GoogleAnalytics extends Component {
-  componentDidMount() {
-    this.logPageChange(this.props.location.pathname, this.props.location.search);
-  }
+const GoogleAnalytics = () => {
+  const location = useLocation();
+  const prevLocation = useRef(location);
 
-  componentDidUpdate({ location: prevLocation }) {
-    const {
-      location: { pathname, search },
-    } = this.props;
-    const isDifferentPathname = pathname !== prevLocation.pathname;
-    const isDifferentSearch = search !== prevLocation.search;
+ // To check config settings
+
+  useEffect(() => {
+    // Initialize Google Analytics on mount
+    if (config.analytics.useGoogleAnalytics) {
+      console.log("INITIALIZE GA");
+      console.log(`Google analytics initialized with config:`, config.analytics);
+      ReactGA.initialize(config.analytics.ga_UA, {
+        debug: false,
+        ...config.analytics.options, // Using config directly instead of options prop
+      });
+    }
+
+    // Log the initial page load
+    const { pathname, search } = location;
+    logPageChange(pathname, search);
+
+    // Store current location
+    prevLocation.current = location;
+  }, []); // Run only on mount
+
+  useEffect(() => {
+    const { pathname, search } = location;
+    const isDifferentPathname = pathname !== prevLocation.current.pathname;
+    const isDifferentSearch = search !== prevLocation.current.search;
 
     if (isDifferentPathname || isDifferentSearch) {
-      this.logPageChange(pathname, search);
+      logPageChange(pathname, search);
     }
-  }
 
-  logPageChange(pathname, search = '') {
+    prevLocation.current = location;
+  }, [location]); // Run whenever location changes
+
+  const logPageChange = (pathname, search = '') => {
     const page = pathname + search;
-    const { location } = window;
+    const { origin } = window.location;
     ReactGA.set({
       page,
-      location: `${location.origin}${page}`,
-      ...this.props.options,
+      location: `${origin}${page}`,
+      ...config.analytics.options, // Using config directly here as well
     });
     ReactGA.pageview(page);
-  }
+  };
 
-  render() {
-    return null;
-  }
-}
-
-GoogleAnalytics.propTypes = {
-  location: PropTypes.shape({
-    pathname: PropTypes.string,
-    search: PropTypes.string,
-  }).isRequired,
-  options: PropTypes.shape({}),
+  return null; // Not rendering any UI, just subscribing to location changes
 };
 
-GoogleAnalytics.defaultProps = {
-  options: {},
-};
 
-const RouteTracker = () => <Route component={GoogleAnalytics} />;
-
-const init = (options = {}) => {
-  if (config.analytics.useGoogleAnalytics) {
-    ReactGA.initialize(config.analytics.ga_UA, {
-      debug: false,
-      ...options,
-    });
-  }
-
-  return config.analytics.useGoogleAnalytics;
-};
-
-// Conditionally track events
-// Just a wrapper for https://github.com/react-ga/react-ga#reactgaeventargs
-const trackEvent = args => {
-  if (config.analytics.useGoogleAnalytics) {
-    ReactGA.event(args);
-  }
-};
-
-export default {
-  GoogleAnalytics,
-  RouteTracker,
-  init,
-  trackEvent,
-};
+export default GoogleAnalytics;
