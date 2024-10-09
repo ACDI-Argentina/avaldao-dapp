9cfe4f0a8206e20790c9f2289111490d75c99b22
Jonatan E. Duttweiler <jonatanduttweiler@gmail.com>
2024-10-09 18:50:52 -0300

    Rewrite component as functional

--- src/components/pages/FaqPage.jsx
+++ src/components/pages/FaqPage.jsx
@@ -1,43 +1,39 @@
-import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import { Web3AppContext } from 'lib/blockchain/Web3App'
-import { withTranslation } from 'react-i18next'
-import Page from './Page'
-import { Grid, Typography } from '@material-ui/core'
-import styles from "assets/jss/material-kit-react/views/componentsSections/aboutViewStyle.js";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
+import Page from './Page';
+import { Grid, Typography } from '@material-ui/core';
+import styles from 'assets/jss/material-kit-react/views/componentsSections/aboutViewStyle.js';
+
+const useStyles = makeStyles(styles);
 
 /**
- * Pantalla de FAQ.
+ * Pantalla de FAQ 
  * 
  */
-class FaqPage extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-
-    const { classes, t, ...rest } = this.props;
-
-    return (
-      <Page>
-        <Grid container spacing={1} style={{ padding: "2em" }}>
-          <Grid item xs={12}>
-            <Typography variant="h5" component="h5">
-              {t('faqTitle')}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1">{t('faqQuestion1')}</Typography>
-            <Typography variant="body1">{t('faqResponse1')}</Typography>
-          </Grid>
+const FaqPage = () => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+    
+  return (
+    <Page>
+      <Grid container spacing={1} style={{ padding: '2em' }}>
+        <Grid item xs={12}>
+          <Typography variant="h5" component="h5" className={classes.title}>
+            {t('faqTitle')}
+          </Typography>
         </Grid>
-      </Page>
-    );
-  }
-}
+        <Grid item xs={12}>
+          <Typography variant="subtitle1" className={classes.subtitle}>
+            {t('faqQuestion1')}
+          </Typography>
+          <Typography variant="body1" className={classes.body}>
+            {t('faqResponse1')}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Page>
+  );
+};
 
-FaqPage.contextType = Web3AppContext;
-
-export default (withStyles(styles)(withTranslation()(FaqPage)));
+export default FaqPage;
