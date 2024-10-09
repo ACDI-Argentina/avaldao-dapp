78c3dd75a90ebea4e344a68251e820c19f19bc27
Jonatan E. Duttweiler <jonatanduttweiler@gmail.com>
2024-10-09 20:30:46 -0300

    Improve connection button

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { selectCurrentUser } from '../redux/reducers/currentUserSlice';
 import { useSelector } from 'react-redux';
 import { Web3AppContext } from 'lib/blockchain/Web3App';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next'; // useTranslation hook
 import { web3Utils } from 'commons';
 import Icon from '@material-ui/core/Icon';
 import { makeStyles } from '@material-ui/core';
@@ -22,25 +22,26 @@ import PrimaryButton from './buttons/PrimaryButton';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import config from 'configuration';
+import "./ConnectButton.scss";
 
 const useStyles = makeStyles({
   walletIcon: {
     height: '100%',
-    marginTop: '-0.4em'
-  }
+    marginTop: '-0.4em',
+  },
 });
 
-const ConnectButton = (props) => {
+const networkNames = {
+  31: "Rsk Testnet",
+  30: "Rsk Mainnet"
+}
+
+const ConnectButton = () => {
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
   const [showAbout, setShowAbout] = useState(false);
   const classes = useStyles();
-  const { t } = props;
-  const {
-    loginAccount,
-    logoutAccount,
-    network,
-    web3,
-  } = useContext(Web3AppContext);
+  const { t } = useTranslation(); // Use useTranslation hook here
+  const { loginAccount, logoutAccount, network, web3 } = useContext(Web3AppContext);
   const currentUser = useSelector(selectCurrentUser);
 
   const isUserConnected = currentUser?.address || false;
@@ -48,70 +49,53 @@ const ConnectButton = (props) => {
   const isUserAdmin = currentUser?.hasRole(config.ADMIN_ROLE) || false;
   const isCorrectNetwork = network?.isCorrect || false;
 
-  const handleOpenMenu = (event) => {
-    setMenuAnchorEl(event.currentTarget);
-  };
+  console.log(currentUser);
 
-  const handleCloseMenu = () => {
-    setMenuAnchorEl(null);
-  };
-
-  const handleClickProfile = () => {
-    history.push(`/profile`);
-  };
-
-  const handleClickMisAvales = () => {
-    history.push(`/mis-avales`);
-  };
-
-  const handleClickUsers = () => {
-    history.push(`/users`);
-  };
-
-  const handleClickAbout = () => {
-    setShowAbout(true);
-    setMenuAnchorEl(null);
-  };
-
-  const handleClickLogout = () => {
-    logoutAccount();
-    setMenuAnchorEl(null);
-  };
-
-  
   return (
     <React.Fragment>
-
       {!isUserConnected && (
         <PrimaryButton
           style={{ width: '15em' }}
-          onClick={() => loginAccount()}>
+          onClick={() => loginAccount()}
+          title={networkNames[web3.networkId] || ""}
+        >
           {t('connectWallet')}
         </PrimaryButton>
       )}
 
       {isUserConnected && (
-        <PrimaryButton
-          title={web3.networkId == 31? "Rsk Testnet": web3.networkId == 30? "Rsk Mainnet": ""}
-          color={isCorrectNetwork ? "success" : "warning"}
-          style={{ width: '15em' }}
-          onClick={handleOpenMenu}
-          startIcon={
-            web3.wallet ?
-              <Icon className={classes.walletIcon}>
-                <img src={web3.wallet.logo}
-                  style={{ width: '100%' }}
-                  alt="connect" />
-              </Icon> :
-              <AccountBalanceWalletIcon />
-          }
-          endIcon={
-            menuAnchorEl ?
-              <ExpandLessIcon></ExpandLessIcon> :
-              <ExpandMoreIcon></ExpandMoreIcon>
-          }>
-          {web3Utils.abbreviateAddress(currentUser?.address)}
-        </PrimaryButton>
+        <div className="connected-wrapper">
+          <PrimaryButton
+            title={networkNames[web3.networkId] || ""}
+            color={isCorrectNetwork ? 'teal' : 'warning'}
+            style={{ width: '15em' }}
+            onClick={(event) => setMenuAnchorEl(event.currentTarget)}
+            endIcon={menuAnchorEl ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          >
+            <div className="connected-content">
+              {web3.wallet ? (
+                <Icon className={classes.walletIcon}>
+                  <img src={web3.wallet.logo} style={{ width: '100%' }} alt="connect" />
+                </Icon>
+              ) : (
+                <AccountBalanceWalletIcon />
+              )}
+
+              <div className="connected-account">
+                {web3Utils.abbreviateAddress(currentUser?.address)}
+                {isUserRegistered &&
+                  <div className="user-details">
+                    <div className="user-details-avatar"> 
+                      <img src={`https://give4forest.org${currentUser.avatarCid}`} alt="" className="small-avatar" />
+                      { currentUser.avatar }
+                      </div>
+                    <div className="user-details-name"> {currentUser.name}</div>
+                  </div>
+                }
+              </div>
+            </div>
+          </PrimaryButton>
+        </div>
       )}
 
       <Menu
@@ -119,66 +103,59 @@ const ConnectButton = (props) => {
         anchorEl={menuAnchorEl}
         keepMounted
         open={Boolean(menuAnchorEl)}
-        onClose={handleCloseMenu}
+        onClose={() => setMenuAnchorEl(null)}
         getContentAnchorEl={null}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         transformOrigin={{ vertical: 'top', horizontal: 'center' }}
       >
-        <MenuItem onClick={
-          handleClickProfile
-        }>
+        <MenuItem onClick={() => history.push('/profile')}>
           <ListItemIcon>
-            <AccountCircleIcon />
+            <AccountCircleIcon style={{ color: '#7868e5' }}/>
           </ListItemIcon>
           <ListItemText primary={isUserRegistered ? t('menuProfile') : t('menuSignup')} />
         </MenuItem>
-        {isUserRegistered &&
-          <MenuItem onClick={
-            handleClickMisAvales
-          }>
+
+        {isUserRegistered && (
+          <MenuItem onClick={() => history.push('/mis-avales')}>
             <ListItemIcon>
-              <ListAltIcon />
+              <ListAltIcon style={{ color: '#7868e5' }}/>
             </ListItemIcon>
             <ListItemText primary={t('misAvalesTitle')} />
-          </MenuItem>}
-        {isUserAdmin &&
-          <MenuItem onClick={
-            handleClickUsers
-          }>
+          </MenuItem>
+        )}
+
+        {isUserAdmin && (
+          <MenuItem onClick={() => history.push('/users')}>
             <ListItemIcon>
-              <PeopleIcon />
+              <PeopleIcon style={{ color: '#7868e5' }}/>
             </ListItemIcon>
             <ListItemText primary={t('usersTitle')} />
-          </MenuItem>}
-        <MenuItem onClick={
-          handleClickAbout
-        }>
+          </MenuItem>
+        )}
+
+        <MenuItem onClick={() => { setShowAbout(true); setMenuAnchorEl(null); }}>
           <ListItemIcon>
-            <InfoIcon />
+            <InfoIcon style={{ color: '#7868e5' }}/>
           </ListItemIcon>
           <ListItemText primary={t('aboutTitle')} />
         </MenuItem>
-        <MenuItem onClick={
-          handleClickLogout
-        }>
+
+        <MenuItem onClick={() => { logoutAccount(); setMenuAnchorEl(null); }}>
           <ListItemIcon>
-            <ExitToAppIcon />
+            <ExitToAppIcon style={{ color: '#7868e5' }}/>
           </ListItemIcon>
           <ListItemText primary={t('logoutTitle')} />
         </MenuItem>
       </Menu>
+
       <About
         fullWidth={true}
         maxWidth="sm"
         open={showAbout}
-        onClose={() => {
-          setShowAbout(false);
-        }}
-      >
-      </About>
-
+        onClose={() => setShowAbout(false)}
+      />
     </React.Fragment>
   );
 };
 
-export default withTranslation()(ConnectButton);
+export default ConnectButton;
