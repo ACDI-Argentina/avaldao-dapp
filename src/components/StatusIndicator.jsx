8113b40ebd0da027c700cd727830f6f5a569a19e
Jonatan E. Duttweiler <jonatanduttweiler@gmail.com>
2024-12-18 17:31:31 -0300

    Update status indicator, previously all of the avales showed the same

--- src/components/StatusIndicator.jsx
+++ src/components/StatusIndicator.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Status } from '@acdi/efem-dapp';
 import Chip from '@material-ui/core/Chip';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -19,8 +20,22 @@ class StatusIndicator extends Component {
         if (status.isLocal) {
             icon = (<CircularProgress size={iconSize} />);
         }
+        if(status.id == 1){
+            icon = (<ErrorOutlineIcon size={iconSize} />);
+        }
+
+        const statusClassMap = {
+            0: classes.solicitado,
+            1: classes.rechazado,
+            2: classes.aceptado,
+            3: classes.vigente,
+            4: classes.finalizado,
+        };
+
+        const dynamicClass = statusClassMap[status.id] || classes.root;
+
         return (
-            <Chip className={classes.root}
+            <Chip className={`${classes.root} ${dynamicClass}`}
                 size="small"
                 label={status.name}
                 color="primary"
@@ -41,11 +56,32 @@ StatusIndicator.defaultProps = {
 const styles = theme => ({
     root: {
         marginTop: '0.5em',
+    },
+    solicitado: {
+        color: '#6C757D',
+        backgroundColor: '#F8F9FA',
+    },
+    rechazado: {
+        color: '#FF0000',
+        backgroundColor: '#FFEBEB',
+    },
+    aceptado: {
         color: '#52D681',
-        borderColor: '#52D681',
         backgroundColor: '#E0FFEB'
-    }
-  });
+    },
+    vigente: {
+        color: '#007BFF',
+        backgroundColor: '#D9EFFF',
+    },
+    finalizado: {
+        color: '#6C757D',
+        backgroundColor: '#E9ECEF',
+    },
+    default: {
+        color: '#AAAAAA',
+        backgroundColor: '#F5F5F5',
+    },
+});
 
 export default withStyles(styles)(
     (StatusIndicator)
